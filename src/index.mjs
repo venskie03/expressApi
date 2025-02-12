29c4bc639dc2d7c0401716c4bff2c51c01a218d3
venskie03 <kevinkyleganados03@gmail.com>
2025-02-12 21:33:29 +0800

    fix cors

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,15 @@
 import express from 'express'
 import axios from 'axios'
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
-const app = express()
+const app = express();
+
+app.use(cors({
+    origin: '*', 
+}));
 
 app.use(express.json())
 
